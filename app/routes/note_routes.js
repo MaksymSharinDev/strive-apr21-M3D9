4c3f57470351be2cbdeee23e363f9e870165664c
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-05-27 16:21:44 +0200

    iteration: front-end retrieve token

---
+++ app/routes/note_routes.js
@@ -0,0 +1,58 @@
+const fetch = require("node-fetch");
+
+
+module.exports = function (app, db) {
+
+
+    app.post('/api/login', async (req, res) => {
+        try {
+            await fetch('https://striveschool-api.herokuapp.com/api/account/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({
+                    "username": `${process.env.STRIVE_EMAIL}`,
+                    "password": `${process.env.STRIVE_PASSWORD}`
+                })
+            }).then(response => response.json()).then(data => res.send(JSON.stringify(data)))
+        } catch (e) {
+            res.send(e)
+        }
+    })
+
+    app.post('/api/product', (req, res) => {
+        let token = 'Bearer ' + req.body['access_token']
+        try {
+            fetch('https://striveschool-api.herokuapp.com/api/product/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+                body: JSON.stringify(req.body)
+            }).then(response => response.json())
+            .then(data => console.log( data ) ) //res.send(data))
+        } catch (e) {
+            res.send(e)
+        }
+    })
+/*
+    app.get('/api/product', (req, res) => {
+        let token = 'Bearer ' + req.body['access_token']
+        try {
+            fetch('https://striveschool-api.herokuapp.com/api/product/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+            }).then(res => res.json()).then(data => { console.log(e) ; res.send(data) })
+        } catch (e) {
+            console.log(e)
+            res.send(e)
+        }
+    })
+*/
+
+};
+
+
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+require('dotenv').config()
+//const MongoClient = require('mongodb').MongoClient
+//const bodyParser = require('body-parser')
+
+const app = express()
+const port = 8000
+app.listen(port, () => console.log('We are live on ' + port) )
+app.use(express.static('public'));
+app.use(express.json())
+require('./app/routes')(app, {})
